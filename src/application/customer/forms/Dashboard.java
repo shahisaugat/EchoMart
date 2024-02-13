3690aee8392b3eba1fa6f6d01234732cf973174a
shahi <devops.shahi@gmail.com>
2024-02-13 20:27:44 +0545

    Application Initiated

---
+++ src/application/customer/forms/Dashboard.java
@@ -0,0 +1,32 @@
+package application.customer.forms;
+
+/**
+ *
+ * @author shahi
+ */
+public class Dashboard extends javax.swing.JPanel {
+
+    public Dashboard() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1038, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 501, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/application/customer/main/EchoMartRunner.java
@@ -0,0 +1,59 @@
+package application.customer.main;
+
+import application.customer.forms.LoginForm;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import java.awt.Dimension;
+
+/**
+ *
+ * @author shahi
+ */
+public class EchoMartRunner extends javax.swing.JFrame {
+    
+    private static EchoMartRunner application;
+    private final LoginForm loginForm;
+
+    public EchoMartRunner() {
+        initComponents();
+        setLocationRelativeTo(null);
+        setSize(new Dimension(1366, 768));
+        
+        loginForm = new LoginForm();
+        setContentPane(loginForm);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        
+        FlatLaf.registerCustomDefaultsSource("application.customer.theme");
+        FlatLightLaf.setup();
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            application = new EchoMartRunner();
+            application.setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
