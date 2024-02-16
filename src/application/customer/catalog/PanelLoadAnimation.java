c4b207f63a76aabaf2dd24ea3b90c4579384f6b8
shahi <devops.shahi@gmail.com>
2024-02-16 21:39:25 +0545

    LoadAnimation Added

--- src/application/customer/catalog/PanelLoadAnimation.java
+++ src/application/customer/catalog/PanelLoadAnimation.java
@@ -1,43 +1,47 @@
 package application.customer.catalog;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author shahi
  */
-public class ContentLabeling extends javax.swing.JPanel {
+public class PanelLoadAnimation extends javax.swing.JPanel {
 
-    public ContentLabeling(String labels) {
+    public PanelLoadAnimation(ImageIcon loadImage) {
         initComponents();
         
-        jLabel1.setText(labels);
+        loadGIF.setIcon(loadImage);
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        loadGIF = new javax.swing.JLabel();
 
-        setBackground(new java.awt.Color(51, 51, 51));
+        setBackground(new java.awt.Color(239, 239, 239));
+        setOpaque(false);
 
-        jLabel1.setFont(new java.awt.Font("Yu Gothic UI", 1, 13)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Add to cart");
+        loadGIF.setIcon(new javax.swing.ImageIcon(getClass().getResource("/application/customer/catalog/blackLoader.gif"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 117, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(loadGIF)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(loadGIF)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel loadGIF;
     // End of variables declaration//GEN-END:variables
 }
