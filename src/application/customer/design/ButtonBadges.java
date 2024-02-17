cbd7941a51a0b74ca84deb6336479e710b2b97e1
shahi <devops.shahi@gmail.com>
2024-02-18 00:07:04 +0545

    Button Badges modified

--- src/application/customer/design/ButtonBadges.java
+++ src/application/customer/design/ButtonBadges.java
@@ -1,124 +1,109 @@
 package application.customer.design;
 
-import java.awt.AlphaComposite;
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.TexturePaint;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import org.jdesktop.animation.timing.Animator;
-import org.jdesktop.animation.timing.TimingTarget;
-import org.jdesktop.animation.timing.TimingTargetAdapter;
+import javax.swing.plaf.basic.BasicButtonUI;
 
+/**
+ *
+ * @author RAVEN
+ */
 public class ButtonBadges extends JButton {
 
-    public int getBadges() {
-        return badges;
+    public Color getBadgeColor() {
+        return badgeColor;
     }
 
-    public void setBadges(int badges) {
-        this.badges = badges;
+    public void setBadgeColor(Color badgeColor) {
+        this.badgeColor = badgeColor;
     }
 
-    public Color getEffectColor() {
-        return effectColor;
-    }
-
-    public void setEffectColor(Color effectColor) {
-        this.effectColor = effectColor;
-    }
-
-    private Animator animator;
-    private int targetSize;
-    private float animatSize;
-    private Point pressedPoint;
-    private float alpha;
-    private Color effectColor = new Color(173, 173, 173);
-    private int badges;
+    private BufferedImage image;
+    private Color badgeColor = new Color(220, 67, 67);
 
     public ButtonBadges() {
-        setContentAreaFilled(false);
-        setBorder(new EmptyBorder(5, 5, 5, 5));
-        setBackground(Color.WHITE);
-        setCursor(new Cursor(Cursor.HAND_CURSOR));
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent me) {
-                targetSize = Math.max(getWidth(), getHeight()) * 2;
-                animatSize = 0;
-                pressedPoint = me.getPoint();
-                alpha = 0.5f;
-                if (animator.isRunning()) {
-                    animator.stop();
-                }
-                animator.start();
-            }
-        });
-        TimingTarget target = new TimingTargetAdapter() {
-            @Override
-            public void timingEvent(float fraction) {
-                if (fraction > 0.5f) {
-                    alpha = 1 - fraction;
-                }
-                animatSize = fraction * targetSize;
-                repaint();
-            }
-        };
-        animator = new Animator(400, target);
-        animator.setResolution(0);
+        setUI(new BadgeButtonUI());
+        setOpaque(false);
+        setForeground(Color.WHITE);
+        setBorder(new EmptyBorder(10, 11, 10, 11));
+        setHorizontalTextPosition(SwingConstants.CENTER);
     }
 
-//    @Override
-//    protected void paintComponent(Graphics grphcs) {
-//        int width = getWidth();
-//        int height = getHeight();
-//        int size = Math.min(width, height) - 8;
-//        int x = (width - size) / 2;
-//        int y = (height - size) / 2;
-//        BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
-//        Graphics2D g2 = img.createGraphics();
-//        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//        g2.setColor(getBackground());
-//        g2.fillOval(0, 0, size, size);
-//        if (pressedPoint != null) {
-//            g2.setColor(effectColor);
-//            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
-//            g2.fillOval((int) (pressedPoint.x - animatSize / 2), (int) (pressedPoint.y - animatSize / 2), (int) animatSize, (int) animatSize);
-//        }
-//        g2.dispose();
-//        grphcs.drawImage(img, x, y, null);
-//        super.paintComponent(grphcs);
-//    }
+    private void createImage() {
+        if (getIcon() != null) {
+            image = new BufferedImage(getIcon().getIconWidth(), getIcon().getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = image.createGraphics();
+            g2.drawImage(((ImageIcon) getIcon()).getImage(), 0, 0, null);
+            g2.dispose();
+        } else {
+            image = null;
+        }
+    }
 
     @Override
-    public void paint(Graphics grphcs) {
-        super.paint(grphcs);
-        if (badges > 0) {
-            String value = badges > 9 ? "+9" : badges + "";
-            int width = getWidth();
-            Graphics2D g2 = (Graphics2D) grphcs;
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            FontMetrics ft = g2.getFontMetrics();
-            Rectangle2D r2 = ft.getStringBounds(value, g2);
-            int fw = (int) r2.getWidth();
-            int fh = (int) r2.getHeight();
+    public void setIcon(Icon icon) {
+        super.setIcon(icon);
+        createImage();
+    }
+
+    private class BadgeButtonUI extends BasicButtonUI {
+
+        @Override
+        protected void paintText(Graphics grphcs, JComponent jc, Rectangle rctngl, String string) {
+
+        }
+
+        @Override
+        protected void paintIcon(Graphics grphcs, JComponent jc, Rectangle iconRect) {
+            if (image != null && !getText().equals("")) {
+                //  Custom Badge Icon
+                Graphics2D g2 = (Graphics2D) grphcs.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                double size = Math.max(iconRect.getWidth(), iconRect.getHeight()) * 0.8f;
+                double x = Math.min(iconRect.getX() + iconRect.getWidth() / 2, jc.getWidth() - size);
+                double y = Math.max(iconRect.getY() - size / 2, 0);
+                Area area = new Area(iconRect);
+                area.subtract(new Area(new Ellipse2D.Double(x, y, size, size)));
+                g2.setPaint(new TexturePaint(image, iconRect));
+                g2.fill(area);
+                String text = getText();
+                createText(g2, x, y, size, text);
+                g2.dispose();
+            } else {
+                super.paintIcon(grphcs, jc, iconRect);
+            }
+        }
+
+        private void createText(Graphics2D g2, double x, double y, double size, String text) {
+            FontMetrics fm = g2.getFontMetrics();
+            Rectangle2D f2d = fm.getStringBounds(text, g2);
+            double space = size * 0.08f;
+            double width = Math.max(size - space * 2, f2d.getWidth() + 10);
+            double height = size - space * 2;
+            g2.setColor(badgeColor);
+            g2.translate(x, y);
+            g2.fill(new RoundRectangle2D.Double(space, space, width, height, height, height));
+            double fx = ((width - f2d.getWidth()) / 2) + space;
+            double fy = ((height - f2d.getHeight()) / 2) + space;
             g2.setColor(getForeground());
-            int size = Math.max(fw, fh);
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 0.9f));
-            g2.fillOval(width - size - 4, 0, size, size);
-            int x = (size - fw) / 2;
-            g2.setColor(Color.WHITE);
-            g2.setComposite(AlphaComposite.SrcOver);
-            g2.drawString(value, width - size + x, ft.getAscent() + 1);
-            g2.dispose();
+            g2.drawString(text, (int) fx, (int) fy + fm.getAscent());
         }
     }
 }
