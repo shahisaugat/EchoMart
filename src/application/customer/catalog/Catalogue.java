8154490923e729d8a404be59a0e5f3d3947df4a3
shahi <devops.shahi@gmail.com>
2024-02-18 23:55:39 +0545

    Catalogue UI Changed

--- src/application/customer/catalog/Catalogue.java
+++ src/application/customer/catalog/Catalogue.java
@@ -1,6 +1,7 @@
 package application.customer.catalog;
 
 import application.customer.design.HoverOver;
+import java.awt.Dimension;
 import javax.swing.ImageIcon;
 
 /**
@@ -9,122 +10,118 @@ import javax.swing.ImageIcon;
  */
 public class Catalogue extends javax.swing.JPanel {
     
-    private HoverOver hoverOver;
+    private final HoverOver hoverOver;
 
     public Catalogue(ImageIcon img, String name, String price, String status, String time) {
         initComponents();
-                
-        revalidate();
-        repaint();
         
         hoverOver = new HoverOver();
+        hoverOver.setSize(new Dimension(150, 90));
+        hoverOver.setVisible(false);
+        add(hoverOver);
         
         productImage.setImage(img);
         productName.setText(name);
         productPrice.setText(price);
         shippingStatus.setText(status);
-        location.setText(time);
+        rating.setText(time);
+        
+        int hoverX = productImage.getX();
+        int hoverY = productImage.getY();
+        hoverOver.setBounds(hoverX, hoverY, hoverOver.getWidth(), hoverOver.getHeight());
+        
+        setComponentZOrder(hoverOver, 0);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panelRound1 = new application.customer.design.PanelRound();
+        panelShadow1 = new application.customer.catalog.PanelShadow();
         productImage = new application.customer.design.PictureHolder();
-        productName = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         productPrice = new javax.swing.JLabel();
-        location = new javax.swing.JLabel();
+        productName = new javax.swing.JLabel();
+        rating = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         shippingStatus = new javax.swing.JLabel();
 
-        panelRound1.setBackground(new java.awt.Color(233, 233, 233));
-        panelRound1.setRoundBottomLeft(8);
-        panelRound1.setRoundBottomRight(8);
-        panelRound1.setRoundTopLeft(8);
-        panelRound1.setRoundTopRight(8);
+        productImage.setImage(new javax.swing.ImageIcon(getClass().getResource("/application/customer/design/Mask group (13).png"))); // NOI18N
+        productImage.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                productImageMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                productImageMouseExited(evt);
+            }
+        });
+        panelShadow1.add(productImage);
+        productImage.setBounds(20, 20, 150, 90);
 
-        productImage.setBackground(new java.awt.Color(255, 255, 255));
-        productImage.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
-        productImage.setImage(new javax.swing.ImageIcon(getClass().getResource("/application/customer/image/loginBackground_Ad.png"))); // NOI18N
-        productImage.setOpaque(true);
+        productPrice.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 13)); // NOI18N
+        productPrice.setText("$ 1200");
+        panelShadow1.add(productPrice);
+        productPrice.setBounds(20, 144, 90, 21);
 
-        productName.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 15)); // NOI18N
-        productName.setText("Necklace");
+        productName.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 13)); // NOI18N
+        productName.setText("Naviforce Watch");
+        panelShadow1.add(productName);
+        productName.setBounds(20, 120, 140, 21);
 
-        jLabel2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 15)); // NOI18N
-        jLabel2.setText("New");
+        rating.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        rating.setIcon(new javax.swing.ImageIcon(getClass().getResource("/application/customer/design/Vector (4).png"))); // NOI18N
+        rating.setText("4.5");
+        panelShadow1.add(rating);
+        rating.setBounds(130, 170, 40, 18);
 
-        productPrice.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
-        productPrice.setText("NRs. 1200");
+        jButton1.setBackground(new java.awt.Color(255, 92, 0));
+        jButton1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 13)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Add to cart");
+        jButton1.setBorderPainted(false);
+        jButton1.setFocusable(false);
+        panelShadow1.add(jButton1);
+        jButton1.setBounds(20, 202, 150, 27);
 
-        location.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
-        location.setText("Ktm");
-
-        shippingStatus.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
-        shippingStatus.setText("Delivery");
-
-        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
-        panelRound1.setLayout(panelRound1Layout);
-        panelRound1Layout.setHorizontalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(productName, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound1Layout.createSequentialGroup()
-                        .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(panelRound1Layout.createSequentialGroup()
-                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(shippingStatus))
-                            .addGroup(panelRound1Layout.createSequentialGroup()
-                                .addComponent(productPrice)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(location, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(14, 14, 14))))
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(productImage, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        panelRound1Layout.setVerticalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(productImage, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(productName)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(productPrice)
-                    .addComponent(location))
-                .addGap(18, 18, 18)
-                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(shippingStatus))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        shippingStatus.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        shippingStatus.setText("Free Delivery");
+        panelShadow1.add(shippingStatus);
+        shippingStatus.setBounds(20, 171, 100, 18);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelRound1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelShadow1, javax.swing.GroupLayout.DEFAULT_SIZE, 188, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelRound1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelShadow1, javax.swing.GroupLayout.DEFAULT_SIZE, 248, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void productImageMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_productImageMouseEntered
+        hoverActive();
+    }//GEN-LAST:event_productImageMouseEntered
+
+    private void productImageMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_productImageMouseExited
+        hoverInactive();
+    }//GEN-LAST:event_productImageMouseExited
+
+    void hoverActive() {
+        hoverOver.setVisible(true);
+    }
+    
+    void hoverInactive() {
+        hoverOver.setVisible(false);
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel location;
-    private application.customer.design.PanelRound panelRound1;
+    private javax.swing.JButton jButton1;
+    private application.customer.catalog.PanelShadow panelShadow1;
     private application.customer.design.PictureHolder productImage;
     private javax.swing.JLabel productName;
     private javax.swing.JLabel productPrice;
+    private javax.swing.JLabel rating;
     private javax.swing.JLabel shippingStatus;
     // End of variables declaration//GEN-END:variables
 }
