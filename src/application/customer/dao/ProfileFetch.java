15b18a7d015cee8b023729fe62b19eb9c653b3a8
shahi <devops.shahi@gmail.com>
2024-02-18 23:52:12 +0545

    Data Backend

---
+++ src/application/customer/dao/ProfileFetch.java
@@ -0,0 +1,42 @@
+package application.customer.dao;
+
+import application.main.database.MySQLConnection;
+import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author shahi
+ */
+public class ProfileFetch extends MySQLConnection {
+    public HashMap<String, Object> fetchCustomerData(String emailAddress) {
+       HashMap<String, Object> customerData = new HashMap<>();
+    
+       try (Connection conn = openConnection()) {
+            String selectQuery = "SELECT c.FullName, c.LastName, c.EmailAddress, cp.ProfilePicture " +
+                               "FROM customers c " +
+                               "JOIN customers_profile cp ON c.EmailAddress = cp.EmailAddress " +
+                               "WHERE c.EmailAddress = ?";
+        
+                try (PreparedStatement ps = conn.prepareStatement(selectQuery)) {
+                ps.setString(1, emailAddress);
+            
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        customerData.put("FullName", rs.getString("FullName"));
+                        customerData.put("LastName", rs.getString("LastName"));
+                        customerData.put("EmailAddress", rs.getString("EmailAddress"));
+                        customerData.put("ProfilePicture", rs.getBytes("ProfilePicture"));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customerData;
+    }
+
+}
