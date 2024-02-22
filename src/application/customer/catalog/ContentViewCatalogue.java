dd6bfab58b3a820431d01f08ae223081d0d65bc7
shahi <devops.shahi@gmail.com>
2024-02-22 13:07:25 +0545

    Catalogue View

--- src/application/customer/catalog/ContentViewCatalogue.java
+++ src/application/customer/catalog/ContentViewCatalogue.java
@@ -20,14 +20,14 @@ public class ContentViewCatalogue extends javax.swing.JPanel {
     private final JDialog descDialog;
     private int fetchedHeaderFav;
     
-    public ContentViewCatalogue(ImageIcon img, String name, String price, String status, String time) {
+    public ContentViewCatalogue(ImageIcon img, String name, String price, String status, String cond) {
         initComponents();
-//        
-//        productImage.setImage(img);
-//        productName.setText(name);
-//        productPrice.setText(price);
-//        shippingStatus.setText(status);
-//        rating.setText(time);
+        
+        productImage.setImage(img);
+        productName.setText(name);
+        productPrice.setText(price);
+        shippingStatus.setText(status);
+        productCondition.setText(cond);
         
         desc = new ProductDescription();
         
@@ -67,14 +67,14 @@ public class ContentViewCatalogue extends javax.swing.JPanel {
     private void initComponents() {
 
         panelRound1 = new application.customer.design.PanelRound();
-        pictureHolder1 = new application.customer.design.PictureHolder();
+        productImage = new application.customer.design.PictureHolder();
         jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        productName = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
+        productPrice = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        productCondition = new javax.swing.JLabel();
+        shippingStatus = new javax.swing.JLabel();
 
         panelRound1.setBackground(new java.awt.Color(244, 244, 244));
         panelRound1.setRoundBottomLeft(8);
@@ -82,33 +82,33 @@ public class ContentViewCatalogue extends javax.swing.JPanel {
         panelRound1.setRoundTopLeft(8);
         panelRound1.setRoundTopRight(8);
 
-        pictureHolder1.setImage(new javax.swing.ImageIcon(getClass().getResource("/application/customer/catalog/image4.png"))); // NOI18N
+        productImage.setImage(new javax.swing.ImageIcon(getClass().getResource("/application/customer/catalog/image4.png"))); // NOI18N
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/application/customer/catalog/Group 20 (1).png"))); // NOI18N
         jButton2.setBorder(null);
         jButton2.setBorderPainted(false);
         jButton2.setContentAreaFilled(false);
 
-        pictureHolder1.setLayer(jButton2, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        productImage.setLayer(jButton2, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        javax.swing.GroupLayout pictureHolder1Layout = new javax.swing.GroupLayout(pictureHolder1);
-        pictureHolder1.setLayout(pictureHolder1Layout);
-        pictureHolder1Layout.setHorizontalGroup(
-            pictureHolder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pictureHolder1Layout.createSequentialGroup()
+        javax.swing.GroupLayout productImageLayout = new javax.swing.GroupLayout(productImage);
+        productImage.setLayout(productImageLayout);
+        productImageLayout.setHorizontalGroup(
+            productImageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, productImageLayout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
-        pictureHolder1Layout.setVerticalGroup(
-            pictureHolder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pictureHolder1Layout.createSequentialGroup()
+        productImageLayout.setVerticalGroup(
+            productImageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(productImageLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jButton2)
                 .addContainerGap(66, Short.MAX_VALUE))
         );
 
-        jLabel1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
-        jLabel1.setText("Naviforce Watch");
+        productName.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
+        productName.setText("Naviforce Watch");
 
         jButton1.setBackground(new java.awt.Color(255, 92, 0));
         jButton1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
@@ -122,17 +122,21 @@ public class ContentViewCatalogue extends javax.swing.JPanel {
             }
         });
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
-        jLabel2.setText("NRs. 1200");
+        productPrice.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        productPrice.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        productPrice.setText("NRs. 1200");
 
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/application/customer/design/Vector (4).png"))); // NOI18N
         jLabel3.setText("4.5");
 
-        jLabel4.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
-        jLabel4.setText("New");
+        productCondition.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
+        productCondition.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        productCondition.setText("Like New");
 
-        jLabel5.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
-        jLabel5.setText("Free Delivery");
+        shippingStatus.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 13)); // NOI18N
+        shippingStatus.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        shippingStatus.setText("Free Delivery");
 
         javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
         panelRound1.setLayout(panelRound1Layout);
@@ -141,37 +145,37 @@ public class ContentViewCatalogue extends javax.swing.JPanel {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound1Layout.createSequentialGroup()
                 .addGap(0, 17, Short.MAX_VALUE)
                 .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(productName, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(panelRound1Layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(productPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(panelRound1Layout.createSequentialGroup()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(productCondition, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel5)))
+                        .addComponent(shippingStatus)))
                 .addGap(16, 16, 16))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound1Layout.createSequentialGroup()
                 .addGap(12, 12, 12)
-                .addComponent(pictureHolder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(productImage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(12, 12, 12))
         );
         panelRound1Layout.setVerticalGroup(
             panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelRound1Layout.createSequentialGroup()
                 .addGap(12, 12, 12)
-                .addComponent(pictureHolder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(productImage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel1)
+                .addComponent(productName)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
+                    .addComponent(productPrice)
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5))
+                    .addComponent(productCondition)
+                    .addComponent(shippingStatus))
                 .addGap(18, 18, 18)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(12, 12, 12))
@@ -207,12 +211,12 @@ public class ContentViewCatalogue extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private application.customer.design.PanelRound panelRound1;
-    private application.customer.design.PictureHolder pictureHolder1;
+    private javax.swing.JLabel productCondition;
+    private application.customer.design.PictureHolder productImage;
+    private javax.swing.JLabel productName;
+    private javax.swing.JLabel productPrice;
+    private javax.swing.JLabel shippingStatus;
     // End of variables declaration//GEN-END:variables
 }
